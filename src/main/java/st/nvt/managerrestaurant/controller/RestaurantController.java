402f5ca8d18586ba8854a1ef3d13cd8e05e9dddc
TungPOk3 <maithanhtungk31107@gmail.com>
2023-11-01 23:15:16 +0700

    UI add restaurant

---
+++ src/main/java/st/nvt/managerrestaurant/controller/RestaurantController.java
@@ -0,0 +1,13 @@
+package st.nvt.managerrestaurant.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RestaurantController {
+    @GetMapping("/add-restaurant")
+    public String showFormAddRestaurant() {
+
+        return "AddRestaurant";
+    }
+}
